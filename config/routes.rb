a12ec7b50377bfc92e904021d4503abf3148a739
ColinW520 <colinhagan@gmail.com>
2018-11-27 13:00:21 -0600

    adds routes for simplifying sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: "static_pages#home"
   devise_for :users
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/admin' => 'devise/sessions#new'
+  end
 end

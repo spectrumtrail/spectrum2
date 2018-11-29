b67aec21f1d723f07ecdbf14c4c93f9a7c93a9d2
ColinW520 <colinhagan@gmail.com>
2018-11-29 14:09:11 -0600

    first pass at some front end stuff

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  layout "_base"
+  layout "static_pages"
 
   def home
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
+  root to: "static_pages#home"
+  match "/home" => redirect("/"), via: [:get]
   match "/privacy" => "static_pages#privacy", via: [:get]
   match "/terms_of_service" => "static_pages#terms_of_service", via: [:get]
   get "static_pages/home"
-  root to: "static_pages#home"
-  devise_for :users
 
+  devise_for :users
   devise_scope :user do
-    get "/users/sign_out" => "devise/sessions#destroy"
     get "/admin" => "devise/sessions#new"
+    get "/users/sign_out" => "devise/sessions#destroy"
   end
 
   namespace :admin do
+    resource :dashboard, controller: 'dashboard'
+    resources :series
     resources :users
   end
 end

945dbfd4eecd716c216f73b37efb6ac92f25600f
ColinW520 <colinhagan@gmail.com>
2018-12-17 20:50:45 -0600

    always show the admin sidebar on load

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,8 +1,12 @@
 class Admin::BaseController < ApplicationController
   layout -> (controller) { controller.request.xhr? ? false : 'admin' }
-  before_action :require_admin!
+  before_action :require_admin!, :set_body_class
 
   protected
+
+  def set_body_class
+    @body_class = "show-sidebar"
+  end
 
   def require_admin!
     unless current_user.present? && current_user.is_admin?

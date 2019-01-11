8521584ddb0268de84b8f95268aa991bdaab9edf
ColinW520 <colinhagan@gmail.com>
2019-01-11 14:30:21 -0600

    update the Series model with validations and images #2

--- app/models/series.rb
+++ app/models/series.rb
@@ -1,3 +1,9 @@
 class Series < ApplicationRecord
   validates :name, presence: true
+  validates :short_description, presence: true
+  validates :long_description, presence: true
+
+  has_one_attached :promo_image
+  has_one_attached :banner_image
+  has_many_attached :gallery_image
 end
